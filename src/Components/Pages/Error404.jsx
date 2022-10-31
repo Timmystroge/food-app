9acbea675e12d89a63d63ee992d5d5e257d58ba9
timmystroge <timmystroge75@gmail.com>
2022-10-31 03:01:35 +0100

    designed popup modals -> product details modal, cart modal, checkout modal. Dashboard Design (75% done), dashboard features (55% done) ; )

--- src/Components/Pages/Error404.jsx
+++ src/Components/Pages/Error404.jsx
@@ -1,9 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 const Error404 = () => {
   return (
-    <div class="container" style="{text-align: center; color: red;}">
-      <h2>404 Not Found</h2>
-      <h4>Oops! E be like say you don lost o :( </h4>
+    <div
+      class="container"
+      style={{
+        textAlign: "center",
+        color: "white",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        lineHeight: "30px"
+      }}
+    >
+      <h2>404 Not Found!</h2>
+      <h4>Oops! E be like say you don lost o : ( </h4>
       <p>Page not found. Kindly go back home</p>
       <Link to="/">Back to Home Page</Link>
     </div>
